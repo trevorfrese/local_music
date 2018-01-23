2428c8eac811ce0ffe03747c80a8cfcf9facffc3
Trevor Frese <trevor.frese@gmail.com>
2018-01-22 18:51:13 -0800

    Update packages

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'));
 app.use(express.static(__dirname + '/public'));
 
 app.use('/spotify', require('./routes/spotify'))
+
+require('./utils/sendgrid');
