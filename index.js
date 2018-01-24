e56f44e7565c2babd887891956159b6d50138631
Trevor Frese <trevor.frese@gmail.com>
2018-01-23 22:22:37 -0800

    lint

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/spotify', require('./routes/spotify'))
+app.use('/spotify', require('./routes/spotify'));
 
 require('./utils/sendgrid');
