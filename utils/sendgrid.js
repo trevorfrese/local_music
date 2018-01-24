65fd19cdb903d7011190292d21615078e580648a
Trevor Frese <trevor.frese@gmail.com>
2018-01-23 22:19:48 -0800

    lint

--- utils/sendgrid.js
+++ utils/sendgrid.js
@@ -1,21 +1,23 @@
 const weeklyRoundUpEmailId = '20d651d6-8b8e-455c-bd34-a293b8582217';
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
-const sgMail = require('@sendgrid/mail');
+const sendgrid = require('@sendgrid/mail');
 const moment = require('moment');
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+sendgrid.setApiKey(SENDGRID_API_KEY);
 
 const msg = {
   to: 'trevor.frese@gmail.com',
   from: 'trevor.frese@gmail.com',
   subject: `Discover Local Music ${moment().format('L')}`,
-  template_id: weeklyRoundUpEmailId
+  template_id: weeklyRoundUpEmailId,
 };
 
-try {
-  sgMail.send(msg);
-  console.log('done');
-} catch (err) {
-  console.log(err);
-}
+(async () => {
+  try {
+    const [resp, body] = await sendgrid.send(msg);
+    console.log('done', resp, body);
+  } catch (err) {
+    console.log(err);
+  }
+})();
