795b9443eb7c829bb1de5b5dc3c687d8387900d8
Kevin Malta <kevin.malta@gmail.com>
2018-03-16 12:16:23 -0700

    Creates separate routes file for spotify routes.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/spotify', require('./routes/routes'));
+app.use('/spotify', require('./routes/spotify'));
 
 require('./utils/sendgrid');
 
