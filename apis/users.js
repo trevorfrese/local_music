587b9c271fbc073c75a814c8e431b9acb4ff5428
Trevor Frese <trevor.frese@gmail.com>
2018-03-24 02:13:33 -0700

    Test endpoint to do look ups per user

--- apis/users.js
+++ apis/users.js
@@ -1,12 +1,14 @@
 const knex = require('knex')(require('../knexfile'));
 const songkick = require('./songkick');
+const spotify = require('./spotify');
 
 async function retrieveUser(userId) {
   const [user] = await knex('user').where('id', userId);
-  console.log(user);
+  console.log('user\n', user, '\n');
   const metroAreaId = await songkick.locationSearch('Los Angeles');
-  // const conc
-  console.log('got results', metroAreaId);
+  console.log('metro area id \n', metroAreaId, '\n');
+  // await songkick.findAllConcerts(metroAreaId);
+  await spotify.buildPlaylist(user.spotifyId, user.accessToken, metroAreaId);
 }
 
 module.exports = {
