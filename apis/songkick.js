718b6bdaebde42a4f7d6ed19e960a36cc5e05a52
Trevor Frese <trevor.frese@gmail.com>
2018-03-23 23:47:08 -0700

    Make improvements across all songkick endpoints

--- apis/songkick.js
+++ apis/songkick.js
@@ -1,10 +1,11 @@
-const SONGKICK_API_KEY = process.env.SONGKICK_API_KEY;
+const { SONGKICK_API_KEY } = process.env;
 const BASE_URL = 'http://api.songkick.com/api/3.0';
 
 const knex = require('knex')(require('../knexfile'));
 
 const querystring = require('querystring');
 const request = require('../utils/request').requestLib;
+const moment = require('moment');
 
 const invokeAPI = async (url) => {
   const [, body] = await request({
@@ -15,11 +16,28 @@ const invokeAPI = async (url) => {
   return body.resultsPage;
 };
 
-const locationSearch = async (query) => {
-  const [metroArea] = await knex('metroArea').where('location', query);
-  if (metroArea) {
-    return metroArea.metroAreaId;
-  }
+const getCalendarParams = (additionalParams = {}) => {
+  const min = moment()
+    .startOf('week')
+    .add('2', 'weeks')
+    .format('YYYY-MM-DD');
+  const max = moment()
+    .startOf('week')
+    .add('2', 'weeks')
+    .endOf('week')
+    .format('YYYY-MM-DD');
+  let params = additionalParams;
+  params = {
+    ...params,
+    apikey: SONGKICK_API_KEY,
+    min_date: min,
+    max_date: max,
+  };
+
+  return querystring.stringify(params);
+};
+
+const getMetroAreaId = async (query) => {
   const params = querystring.stringify({
     apikey: SONGKICK_API_KEY,
     query,
@@ -28,7 +46,16 @@ const locationSearch = async (query) => {
   const url = `${BASE_URL}/search/locations.json?${params}`;
   const result = await invokeAPI(url);
   const [location] = result && result.results && result.results.location;
-  const metroAreaId = location && location.metroArea && location.metroArea.id;
+  return location && location.metroArea && location.metroArea.id;
+};
+
+const locationSearch = async (query) => {
+  const [metroArea] = await knex('metroArea').where('location', query);
+  if (metroArea) {
+    return metroArea.metroAreaId;
+  }
+
+  const metroAreaId = await getMetroAreaId(query);
   await knex('metroArea').insert({
     location: query,
     metroAreaId,
@@ -36,24 +63,26 @@ const locationSearch = async (query) => {
   return metroAreaId;
 };
 
-async function getCalendarPage(metroAreaId, pageNumber) {
-  const [, body] = await request({
-    method: 'GET',
-    url: `http://api.songkick.com/api/3.0/metro_areas/${metroAreaId}/calendar.json?${querystring.stringify({
-      apikey: SONGKICK_API_KEY,
-      page: pageNumber,
-    })}`,
-    json: true,
-  });
-  return body;
+async function getPageTotal(metroAreaId) {
+  const params = getCalendarParams();
+  const url = `${BASE_URL}/metro_areas/${metroAreaId}/calendar.json?${params}`;
+  const resultsPage = await invokeAPI(url);
+  return parseInt(resultsPage.totalEntries / resultsPage.perPage, 10);
+}
+
+async function getCalendarPage(metroAreaId, page) {
+  const params = getCalendarParams({ page });
+  const url = `${BASE_URL}/metro_areas/${metroAreaId}/calendar.json?${params}`;
+  return invokeAPI(url);
 }
 
 async function getSongkickCalendar(metroAreaId, pageTotal) {
   let calendar = [];
   // TO MAKE IT RUN FAST SET pageTotal = 1
+  pageTotal = 1
   for (let i = 1; i < pageTotal + 1; i += 1) {
     console.log('on page ', i);
-    const result = (await getCalendarPage(metroAreaId, i)).resultsPage;
+    const result = await getCalendarPage(metroAreaId, i);
     const page = result && result.results && result.results.event;
     calendar = calendar.concat(page);
   }
@@ -72,36 +101,25 @@ function parseArtistsFromCalendar(calendar) {
   return artists;
 }
 
-async function getPageTotal(metroAreaId) {
-  const [, body] = await request({
-    method: 'GET',
-    url: `http://api.songkick.com/api/3.0/metro_areas/${metroAreaId}/calendar.json?${querystring.stringify({
-      apikey: SONGKICK_API_KEY,
-    })}`,
-    json: true,
-  });
-
-  return parseInt(body.resultsPage.totalEntries / body.resultsPage.perPage, 10);
-}
-
-async function getLocalArtists(metroAreaId) {
+const getLocalArtists = async (metroAreaId) => {
   const pageTotal = await getPageTotal(metroAreaId);
   console.log('page total', pageTotal);
   const calendar = await getSongkickCalendar(metroAreaId, pageTotal);
   const artists = parseArtistsFromCalendar(calendar);
   return artists;
-}
+};
 
-async function getBayAreaArtists() {
-  try {
-    const BAY_AREA_METRO_ID = 26330;
-    const artists = await getLocalArtists(BAY_AREA_METRO_ID);
-    console.log(artists);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function getBayAreaArtists() {
+//   try {
+//     const BAY_AREA_METRO_ID = 26330;
+//     const artists = await getLocalArtists(BAY_AREA_METRO_ID);
+//     console.log(artists);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 module.exports = {
   locationSearch,
+  getLocalArtists,
 };
