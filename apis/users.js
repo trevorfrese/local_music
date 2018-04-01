45f7e4edf0d260adad7cbf657d33edb53a804c0e
Trevor Frese <trevor.frese@gmail.com>
2018-03-31 21:11:47 -0700

    get user genres from db

--- apis/users.js
+++ apis/users.js
@@ -2,13 +2,22 @@ const knex = require('knex')(require('../knexfile'));
 const songkick = require('./songkick');
 const spotify = require('./spotify');
 
+const getUserGenres = async (userId) => {
+  const genres = await knex('genre')
+    .select('genre.name')
+    .innerJoin('userGenre', 'genre.id', 'userGenre.genreId')
+    .where('userId', userId);
+  return genres.map(genre => genre.name);
+};
+
 async function retrieveUser(userId) {
   const [user] = await knex('user').where('id', userId);
   console.log('user\n', user, '\n');
   const metroAreaId = await songkick.locationSearch('Los Angeles');
   console.log('metro area id \n', metroAreaId, '\n');
   // await songkick.findAllConcerts(metroAreaId);
-  await spotify.buildPlaylist(user.spotifyId, metroAreaId, user.accessToken);
+  const genres = await getUserGenres(userId);
+  await spotify.buildPlaylist(user.spotifyId, metroAreaId, genres, user.accessToken);
 }
 
 module.exports = {
